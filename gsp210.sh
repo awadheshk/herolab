fcd17479d54b92c6f4d2b64d76dc661b9e4a0f86
awadheshk <54056550+awadheshk@users.noreply.github.com>
2025-01-17 20:42:32 +0530

    Update gsp210.sh

--- gsp210.sh
+++ gsp210.sh
@@ -1,43 +1,9 @@
 
-export ZONE_2=
 curl -LO raw.githubusercontent.com/awadheshk/herolab/refs/heads/main/gsp210.sh
 sudo chmod +x gsp210.sh
 ./gsp210.sh
 
-
-
-
-#!/bin/bash
-# Define color variables
-
-BLACK=`tput setaf 0`
-RED=`tput setaf 1`
-GREEN=`tput setaf 2`
-YELLOW=`tput setaf 3`
-BLUE=`tput setaf 4`
-MAGENTA=`tput setaf 5`
-CYAN=`tput setaf 6`
-WHITE=`tput setaf 7`
-
-BG_BLACK=`tput setab 0`
-BG_RED=`tput setab 1`
-BG_GREEN=`tput setab 2`
-BG_YELLOW=`tput setab 3`
-BG_BLUE=`tput setab 4`
-BG_MAGENTA=`tput setab 5`
-BG_CYAN=`tput setab 6`
-BG_WHITE=`tput setab 7`
-
-BOLD=`tput bold`
-RESET=`tput sgr0`
-#----------------------------------------------------start--------------------------------------------------#
-
-echo "${BG_MAGENTA}${BOLD}Starting Execution${RESET}"
-
-
-export ZONE_1=$(gcloud compute project-info describe \
---format="value(commonInstanceMetadata.items[google-compute-default-zone])")
-
+export ZONE_1=
 export ZONE_2=
 
 gcloud compute networks create mynetwork \
@@ -76,4 +42,3 @@ gcloud compute instances create mynet-second-vm \
   --labels=goog-ec-src=vm_add-gcloud \
   --reservation-affinity=any
 
-echo "${BG_RED}${BOLD}Completing The Lab !!!${RESET}"
