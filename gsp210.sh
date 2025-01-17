8b233b7cd8866e4f711785bfaec0a6808c4ae158
awadheshk <54056550+awadheshk@users.noreply.github.com>
2025-01-17 21:03:54 +0530

    Update gsp210.sh

--- gsp210.sh
+++ gsp210.sh
@@ -1,6 +1,3 @@
-curl -LO https://raw.githubusercontent.com/awadheshk/herolab/refs/heads/main/gsp210.sh
-sudo chmod +x gsp210.sh
-./gsp210.sh
 
 export ZONE_1=europe-west1-c
 export ZONE_2=us-central1-c
