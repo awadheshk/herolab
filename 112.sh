748be251bd7ff4f9cafda4b169aac30fd4f306ee
awadheshk <54056550+awadheshk@users.noreply.github.com>
2025-01-17 16:45:04 +0530

    Update and rename 079.sh to 112.sh

---
+++ 112.sh
@@ -0,0 +1,26 @@
+
+curl -LO raw.githubusercontent.com/quiccklabs/Labs_solutions/master/Web%20Security%20Scanner%20Qwik%20Start/quicklabgsp112.sh
+
+sudo chmod +x 112.sh
+
+./112.sh
+=============
+export REGION=
+
+gsutil -m cp -r gs://spls/gsp067/python-docs-samples .
+cd python-docs-samples/appengine/standard_python3/hello_world
+
+cat > requirements.txt <<EOF_END
+Flask==1.1.2
+itsdangerous==2.0.1
+Jinja2==3.0.3
+werkzeug==2.0.1
+EOF_END
+
+cat > app.yaml <<EOF_END
+runtime: python39
+EOF_END
+
+gcloud app create --region=$REGION
+
+gcloud app deploy --quiet
