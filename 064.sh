96e6173b9653895aea2c7c03b33d70c0ae3321d6
awadheshk <54056550+awadheshk@users.noreply.github.com>
2025-01-19 09:40:02 +0530

    Update 064.sh

--- 064.sh
+++ 064.sh
@@ -1,3 +1,7 @@
+curl -LO https://raw.githubusercontent.com/awadheshk/herolab/refs/heads/main/P079.sh
+sudo chmod +x P079.sh
+./P079.sh
+
 export USERNAME_2=student-00-48d05c766a37@qwiklabs.net
 gsutil mb -l us -b on gs://$DEVSHELL_PROJECT_ID
 echo "subscribe to quicklab " > sample.txt
